13353b8a1d81349d71df802134e8662160958029
Ndiforamang Fusi <fusisoft@gmail.com>
2024-10-02 07:15:53 -0500

    Deploying vpc and eks

--- modules/terraform_node/userdata.sh
+++ modules/terraform_node/userdata.sh
@@ -5,6 +5,7 @@ sudo apt-get update -y
 sudo apt-get install openjdk-21-jdk -y
 
 #Installing aws cli
+sudo apt-get install unzip -y
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
 unzip awscliv2.zip
 sudo ./aws/install
