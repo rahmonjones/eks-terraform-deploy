ddbb12677acfe2c81328f224f55ff09d296a3663
Ndiforamang Fusi <fusisoft@gmail.com>
2024-10-07 17:53:44 -0500

    Restructured project

--- main.tf
+++ main.tf
@@ -103,6 +103,12 @@ module "maven-sonarqube" {
   vpc_security_group_ids = [var.security_group_id]
   subnet_id              = var.subnet_id
   # main-region   = var.main-region
+
+  db_name                  = var.db_name
+  username                 = var.db_username
+  password                 = var.db_password
+  vpc_security_group_ids   = [var.db_security_group_id]
+  db_subnet_group_name     = var.db_subnet_group
 }
 
 # # module "s3_dynamodb" {
