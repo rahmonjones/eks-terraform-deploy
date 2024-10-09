a6b05d19730ecfef9063220a44f0387d15c95654
Ndiforamang Fusi <fusisoft@gmail.com>
2024-10-08 19:25:21 -0500

    Restructured project

--- main.tf
+++ main.tf
@@ -95,8 +95,8 @@ module "terraform_node" {
   main-region   = var.main-region
 }
 
-module "maven-sonarqube" {
-  source            = "./modules/maven-sonarqube"
+module "maven-sonarqube-server" {
+  source            = "./modules/maven-sonarqube-server"
   ami_id            = var.ami_id
   instance_type     = var.instance_type
   key_name          = var.key_name
