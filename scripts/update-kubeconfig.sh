2d22bc1e6c79afa6190bbb13a06a18589e6ecdcc
rahmonjones <rahmon.jay1@gmail.com>
2024-10-17 23:59:42 +0100

    scripts updated

--- scripts/update-kubeconfig.sh
+++ scripts/update-kubeconfig.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 response="$(aws eks list-clusters --region us-west-2 --output text | grep -i eks-xplur-cluster 2>&1)" 
 if [[ $? -eq 0 ]]; then
-    echo "Success: xplur-cluster exist"
+    echo "Success: eks-xplur-cluster exist"
     aws eks --region us-west-2 update-kubeconfig --name eks-xplur-cluster && export KUBE_CONFIG_PATH=~/.kube/config
 
 else
-    echo "Error: xplur-cluster does not exist"
+    echo "Error: eks-xplur-cluster does not exist"
 fi
