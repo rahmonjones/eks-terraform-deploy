f6a1f9c24e4c75af534a959e029f9e027feedd64
rahmonjones <rahmon.jay1@gmail.com>
2024-10-18 10:30:02 +0100

    updated scripts

--- scripts/update-kubeconfig.sh
+++ scripts/update-kubeconfig.sh
@@ -16,3 +16,14 @@ else
     echo "Try again"
     exit 1
 fi
+
+
+# #!/bin/bash
+# response="$(aws eks list-clusters --region eu-west-2 --output text | grep -i Atoktech-cluster 2>&1)" 
+# if [[ $? -eq 0 ]]; then
+#     echo "Success: Atoktech-cluster exist"
+#     aws eks --region eu-west-2 update-kubeconfig --name Atoktech-cluster && export KUBE_CONFIG_PATH=~/.kube/config
+
+# else
+#     echo "Error: Atoktech-cluster does not exist"
+# fi
